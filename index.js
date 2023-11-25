4ec4bf75b869c41f6ac91fe627ed7a289c55e5a7
promitSDev <129307667+promitSDev@users.noreply.github.com>
2023-11-25 14:06:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,31 +12,40 @@ function makeSound(key){
         case 'w':
             var audio = new Audio("sounds/tom-1.mp3");
             audio.play();
+            document.querySelector("footer").textContent="noiceeee";
             break;
         case 'a':
             var audio = new Audio("sounds/tom-2.mp3");
             audio.play();
+            document.querySelector("footer").textContent="lovely";
             break;
         case 's':
             var audio = new Audio("sounds/tom-3.mp3");
             audio.play();
+            document.querySelector("footer").textContent="wowww";
             break;
         case 'd':
             var audio = new Audio("sounds/tom-4.mp3");
             audio.play();
+            document.querySelector("footer").textContent="let's have some beat";
             break;
         case 'j':
             var audio = new Audio("sounds/snare.mp3");
             audio.play();
+            document.querySelector("footer").textContent="beautiful";
             break;
         case 'k':
             var audio = new Audio("sounds/crash.mp3");
             audio.play();
+            document.querySelector("footer").textContent="hoorraayy";
             break;
         case 'l':
             var audio = new Audio("sounds/kick-bass.mp3");
             audio.play();
+            document.querySelector("footer").textContent="excellent";
             break;
+        default:
+            document.querySelector("footer").textContent="oops you missed the drum"
      }
 }
 
